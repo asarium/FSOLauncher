194ed3ea116fbd1b1430f76cb8cdf6db6a473a6d
asarium <asarium@gmail.com>
2014-08-10 18:26:51 +0200

    Add utility classes

---
+++ UI/WPF/UI.WPF.Launcher.Common/Classes/ReactiveObjectBase.cs
@@ -0,0 +1,19 @@
+﻿#region Usings
+
+using System.Runtime.CompilerServices;
+using FSOManagement.Annotations;
+using ReactiveUI;
+
+#endregion
+
+namespace UI.WPF.Launcher.Common.Classes
+{
+    public abstract class ReactiveObjectBase : ReactiveObject
+    {
+        [NotifyPropertyChangedInvocator]
+        protected void RaiseAndSetIfPropertyChanged<T>(ref T obj, T value, [CallerMemberName] string propertyName = null)
+        {
+            this.RaiseAndSetIfChanged(ref obj, value, propertyName);
+        }
+    }
+}
---
+++ UI/WPF/UI.WPF.Launcher.Common/Util/ProfileManagerExtensions.cs
@@ -0,0 +1,76 @@
+﻿#region Usings
+
+using System;
+using System.Linq.Expressions;
+using System.Reactive.Linq;
+using FSOManagement.Interfaces;
+using ReactiveUI;
+using UI.WPF.Launcher.Common.Interfaces;
+
+#endregion
+
+namespace UI.WPF.Launcher.Common.Util
+{
+    public static class ProfileManagerExtensions
+    {
+        public static IObservable<IProfile> GetProfileObservable(this IProfileManager profileManager)
+        {
+            if (profileManager == null || profileManager.CurrentProfileObservable == null)
+            {
+                return Observable.Empty<IProfile>();
+            }
+            else
+            {
+                return profileManager.CurrentProfileObservable;
+            }
+        }
+
+        public static void CreateProfileBinding<TProfileProperty, TViewModel, TVmProperty>(this IProfileManager profileManager,
+            Expression<Func<IProfile, TProfileProperty>> profileProperty, Func<TProfileProperty, TVmProperty> profileToVmConverter,
+            TViewModel viewModel, Expression<Func<TViewModel, TVmProperty>> rightProperty, Func<TVmProperty, TProfileProperty> vmToProfileConverter)
+        {
+            IDisposable firstBinding = null;
+            IDisposable secondBinding = null;
+
+            profileManager.GetProfileObservable().Subscribe(profile =>
+            {
+                if (firstBinding != null)
+                {
+                    firstBinding.Dispose();
+                }
+                if (secondBinding != null)
+                {
+                    secondBinding.Dispose();
+                }
+
+                firstBinding = profile.WhenAny(profileProperty, val => profileToVmConverter(val.Value)).BindTo(viewModel, rightProperty);
+
+                secondBinding = viewModel.WhenAny(rightProperty, val => vmToProfileConverter(val.Value)).BindTo(profile, profileProperty);
+            });
+        }
+
+        public static void CreateProfileValueBinding<TProfileProperty, TViewModel, TVmProperty>(this IProfileManager profileManager,
+            Expression<Func<IProfile, TProfileProperty>> profileProperty,
+            TViewModel viewModel, Expression<Func<TViewModel, TVmProperty>> rightProperty)
+        {
+            IDisposable firstBinding = null;
+            IDisposable secondBinding = null;
+
+            profileManager.GetProfileObservable().Subscribe(profile =>
+            {
+                if (firstBinding != null)
+                {
+                    firstBinding.Dispose();
+                }
+                if (secondBinding != null)
+                {
+                    secondBinding.Dispose();
+                }
+
+                firstBinding = profile.WhenAnyValue(profileProperty).BindTo(viewModel, rightProperty);
+
+                secondBinding = viewModel.WhenAnyValue(rightProperty).BindTo(profile, profileProperty);
+            });
+        }
+    }
+}
