d6f1cbee89e90c4aba47d17ccf8855ffb61b1dfd
asarium <asarium@gmail.com>
2014-10-01 13:36:28 +0200

    Test the squirrel.windows updater

---
+++ Tests/UI.WPF.Modules.Update.Tests/Services/SquirrelUpdateServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+using UI.WPF.Modules.Update.Services;
+
+namespace UI.WPF.Modules.Update.Tests.Services
+{
+    [TestFixture]
+    public class SquirrelUpdateServiceTests
+    {
+        [Test]
+        public void TestIsUpdatePossible()
+        {
+            var instance = new SquirrelUpdateService();
+
+            Assert.IsFalse(instance.IsUpdatePossible);
+        }
+
+        [Test]
+        public async Task TestCheckForUpdateAsync()
+        {
+            var instance = new SquirrelUpdateService();
+
+            await instance.CheckForUpdateAsync();
+        }
+    }
+}
---
+++ UI/WPF/Modules/UI.WPF.Modules.Update/Services/SquirrelUpdateService.cs
@@ -0,0 +1,59 @@
+﻿#region Usings
+
+using System;
+using System.ComponentModel.Composition;
+using System.Threading.Tasks;
+using Squirrel;
+using UI.WPF.Launcher.Common.Classes;
+using UI.WPF.Launcher.Common.Services;
+
+#endregion
+
+namespace UI.WPF.Modules.Update.Services
+{
+    [Export(typeof(IUpdateService))]
+    public class SquirrelUpdateService : ReactiveObjectBase, IUpdateService
+    {
+        #region IUpdateService Members
+
+        public bool IsUpdatePossible
+        {
+            get
+            {
+                using (var mgr = new UpdateManager(@"http://localhost/squirrel", "FSOLauncher", FrameworkVersion.Net45))
+                {
+                    Console.WriteLine(mgr.RootAppDirectory);
+                }
+
+                return false;
+            }
+        }
+
+        public async Task<IUpdateStatus> CheckForUpdateAsync()
+        {
+            try
+            {
+                using (var mgr = new UpdateManager(@"http://localhost/squirrel", "FSOLauncher", FrameworkVersion.Net45))
+                {
+                    // Use updateManager
+                    var updateInfo = await mgr.CheckForUpdate(false, Console.WriteLine);
+
+                    Console.WriteLine(updateInfo);
+                }
+            }
+            catch (InvalidOperationException)
+            {
+                return new UpdateStatus(null);
+            }
+
+            return null;
+        }
+
+        public Task DoUpdateAsync(IProgress<IUpdateProgress> progressReporter)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
