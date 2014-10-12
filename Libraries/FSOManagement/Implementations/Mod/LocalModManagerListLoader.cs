ee11101927a8943c91f62bea68b3bcdcdf612f5b
asarium <asarium@gmail.com>
2014-10-12 21:46:18 +0200

    Try using MEF to get loader instances

---
+++ Libraries/FSOManagement/Implementations/Mod/LocalModManagerListLoader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Threading.Tasks;
+using FSOManagement.Annotations;
+using FSOManagement.Interfaces.Mod;
+using ModInstallation.Interfaces;
+
+namespace FSOManagement.Implementations.Mod
+{
+    [Export(typeof(IModListLoader))]
+    public class LocalModManagerListLoader : IModListLoader
+    {
+        [NotNull, Import]
+        private ILocalModManager LocalModManager { get; set; }
+
+        public Task<IEnumerable<IModification>> LoadModificationListAsync(string searchFolder)
+        {
+            var currentDirectory = LocalModManager.PackageDirectory;
+        }
+    }
+}
