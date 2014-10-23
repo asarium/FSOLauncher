cf75a9dd21f762d34319c9a4b1dab3f3e18efc6f
asarium <asarium@gmail.com>
2014-10-23 19:24:18 +0200

    Add status to show the changelog

---
+++ UI/WPF/Modules/UI.WPF.Modules.Update/ViewModels/ChangeLogStatus.cs
@@ -0,0 +1,24 @@
+﻿#region Usings
+
+using System;
+using System.Collections.Generic;
+using System.Windows.Input;
+using FSOManagement.Annotations;
+using ReactiveUI;
+using UI.WPF.Modules.Update.Views;
+
+#endregion
+
+namespace UI.WPF.Modules.Update.ViewModels
+{
+    public class ChangeLogStatus
+    {
+        public ChangeLogStatus([NotNull] IEnumerable<KeyValuePair<Version, string>> releaseNotes, [NotNull] IInteractionService interactionService)
+        {
+            OpenChangeLogCommand = ReactiveCommand.CreateAsyncTask(async _ => await interactionService.ShowDialog(new ChangelogDialog(releaseNotes)));
+        }
+
+        [NotNull]
+        public ICommand OpenChangeLogCommand { get; private set; }
+    }
+}
