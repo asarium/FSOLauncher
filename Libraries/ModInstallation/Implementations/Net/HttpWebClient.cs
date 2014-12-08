0e7aff33388a10b8447d719e1def07e3acddb708
asarium <asarium@gmail.com>
2014-12-08 11:34:46 +0100

    Use HttpWebRequest for downloads

--- Libraries/ModInstallation/Implementations/Net/HttpWebClient.cs
+++ Libraries/ModInstallation/Implementations/Net/HttpWebClient.cs
@@ -2,11 +2,12 @@
 
 using System;
 using System.ComponentModel.Composition;
+using System.IO;
 using System.Net;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
-using FSOManagement.Annotations;
+using System.Web;
 using ModInstallation.Interfaces.Net;
 
 #endregion
@@ -16,13 +17,63 @@ namespace ModInstallation.Implementations.Net
     [Export(typeof(IWebClient))]
     public class HttpWebClient : CachingWebClient
     {
+        private const int BufferSize = 96 * 1024;
+
         public override async Task DownloadAsync(Uri uri, string destination, Action<DownloadProgress> downloadReporter, CancellationToken token)
         {
-            using (var client = new DownloadTaskProvider())
+            var request = WebRequest.CreateHttp(uri);
+
+            request.Method = "GET";
+            // Use a dummy User-Agent
+            request.UserAgent = "Mozilla/5.0 (X11; Linux x86_64; rv:34.0) Gecko/20100101 Firefox/28.0";
+
+            using (var response = (HttpWebResponse) await request.GetResponseAsync())
             {
-                token.Register(client.Cancel);
-                await client.StartDownload(uri, destination, downloadReporter);
+                if (response.StatusCode < HttpStatusCode.OK || response.StatusCode >= HttpStatusCode.MultipleChoices)
+                {
+                    throw new HttpException((int) response.StatusCode, "Request failed!");
+                }
+
+                var total = response.ContentLength;
+                var buffer = new byte[BufferSize];
+                using (var responseStream = response.GetResponseStream())
+                {
+                    if (responseStream == null)
+                    {
+                        throw new HttpException("No response Stream available");
+                    }
+
+                    using (var fileStream = OpenFileStream(destination))
+                    {
+                        var current = 0L;
+                        int read;
+                        while ((read = await responseStream.ReadAsync(buffer, 0, buffer.Length, token)) != 0)
+                        {
+                            current += read;
+                            token.ThrowIfCancellationRequested();
+
+                            await fileStream.WriteAsync(buffer, 0, read, token);
+
+                            downloadReporter(new DownloadProgress
+                            {
+                                Current = current,
+                                Total = total
+                            });
+                        }
+                    }
+                }
             }
+        }
+
+        private static Stream OpenFileStream(string destination)
+        {
+            var directory = Path.GetDirectoryName(destination);
+            if (directory != null)
+            {
+                Directory.CreateDirectory(directory);
+            }
+
+            return new FileStream(destination, FileMode.Create, FileAccess.Write, FileShare.ReadWrite, BufferSize, FileOptions.Asynchronous);
         }
 
         protected override async Task<string> ActuallyGetStringAsync(Uri uri, CancellationToken token)
@@ -33,72 +84,5 @@ namespace ModInstallation.Implementations.Net
                 return await responseMessage.Content.ReadAsStringAsync();
             }
         }
-
-        #region Nested type: DownloadTaskProvider
-
-        private class DownloadTaskProvider : IDisposable
-        {
-            private bool _disposed;
-
-            private readonly WebClient _client;
-
-            public DownloadTaskProvider()
-            {
-                _client = new WebClient();
-            }
-
-            #region IDisposable Members
-
-            public void Dispose()
-            {
-                if (_client != null)
-                {
-                    _client.Dispose();
-                }
-
-                _disposed = true;
-            }
-
-            #endregion
-
-            public void Cancel()
-            {
-                if (_client != null && !_disposed)
-                {
-                    _client.CancelAsync();
-                }
-            }
-
-            [NotNull]
-            public Task StartDownload([NotNull] Uri uri, [NotNull] string destination, [NotNull] Action<DownloadProgress> downloadReporter)
-            {
-                _client.DownloadProgressChanged += (sender, args) => downloadReporter(new DownloadProgress
-                {
-                    Current = args.BytesReceived,
-                    Total = args.TotalBytesToReceive
-                });
-
-                var tcs = new TaskCompletionSource<bool>();
-                _client.DownloadFileCompleted += (sender, args) =>
-                {
-                    if (args.Cancelled)
-                    {
-                        tcs.TrySetCanceled();
-                    }
-                    else if (args.Error != null)
-                    {
-                        tcs.TrySetException(args.Error);
-                    }
-                    else
-                    {
-                        tcs.TrySetResult(true);
-                    }
-                };
-
-                return Task.WhenAll(Task.Run(() => _client.DownloadFileAsync(uri, destination)), tcs.Task);
-            }
-        }
-
-        #endregion
     }
 }
