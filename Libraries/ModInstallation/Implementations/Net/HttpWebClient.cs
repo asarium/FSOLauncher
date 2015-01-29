027f05b1c3bd39557b3d17b3b4025123adb3383c
asarium <asarium@gmail.com>
2015-01-29 18:51:33 +0100

    Add a default certificate verification function

--- Libraries/ModInstallation/Implementations/Net/HttpWebClient.cs
+++ Libraries/ModInstallation/Implementations/Net/HttpWebClient.cs
@@ -5,6 +5,8 @@ using System.ComponentModel.Composition;
 using System.IO;
 using System.Net;
 using System.Net.Http;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Web;
@@ -19,6 +21,56 @@ namespace ModInstallation.Implementations.Net
     public class HttpWebClient : CachingWebClient
     {
         private const int BufferSize = 96 * 1024;
+
+        static HttpWebClient()
+        {
+            // Initialize the SSL validation callback
+            ServicePointManager.ServerCertificateValidationCallback = ServerCertificateValidationCallback;
+        }
+
+        private static bool ServerCertificateValidationCallback(object sender,
+            X509Certificate certificate,
+            X509Chain chain,
+            SslPolicyErrors sslPolicyErrors)
+        {
+            // If the certificate is a valid, signed certificate, return true.
+            if (sslPolicyErrors == SslPolicyErrors.None)
+            {
+                return false;
+            }
+
+            // If there are errors in the certificate chain, look at each error to determine the cause.
+            if ((sslPolicyErrors & SslPolicyErrors.RemoteCertificateChainErrors) != 0)
+            {
+                if (chain == null)
+                {
+                    return true;
+                }
+
+                foreach (var status in chain.ChainStatus)
+                {
+                    if ((certificate.Subject == certificate.Issuer) && (status.Status == X509ChainStatusFlags.UntrustedRoot))
+                    {
+                        // Self-signed certificates with an untrusted root are valid.
+                        continue;
+                    }
+                    
+                    if (status.Status != X509ChainStatusFlags.NoError)
+                    {
+                        // If there are any other errors in the certificate chain, the certificate is invalid,
+                        // so the method returns false.
+                        return false;
+                    }
+                }
+                // When processing reaches this line, the only errors in the certificate chain are
+                // untrusted root errors for self-signed certificates. These certificates are valid
+                // for default Exchange Server installations, so return true.
+                return true;
+            }
+            
+            // In all other cases, return false.
+            return false;
+        }
 
         public override async Task DownloadAsync(Uri uri, string destination, Action<DownloadProgress> downloadReporter, CancellationToken token)
         {
