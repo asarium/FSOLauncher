ad4ca56f7c00007a5763193233565a69ef92b1c8
asarium <asarium@gmail.com>
2015-03-24 18:27:04 +0100

    Add IInstallationState

---
+++ UI/WPF/Modules/UI.WPF.Modules.Installation/Interfaces/IInstallationState.cs
@@ -0,0 +1,13 @@
+﻿#region Usings
+
+using System.Threading.Tasks;
+
+#endregion
+
+namespace UI.WPF.Modules.Installation.Interfaces
+{
+    public interface IInstallationState
+    {
+        Task<IInstallationState> WaitForNextState();
+    }
+}
